b9b46520800d823e1bff6184aa8b1ebe12599565
Winsmarl <110825438+winsmarlcanete@users.noreply.github.com>
2025-03-04 00:06:35 +0800

    added javafx lib

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private static Stage primaryStage;
+
+    @Override
+    public void start(Stage stage) {
+        primaryStage = stage;
+        switchScreen("HomeScreen.fxml");  // Start with HomeScreen
+
+        primaryStage.setTitle("JavaFX Multi-Screen App");
+        primaryStage.show();
+    }
+
+    public static void switchScreen(String fxmlFile) {
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("/screens/" + fxmlFile));
+            Scene scene = new Scene(loader.load());
+            primaryStage.setScene(scene);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
