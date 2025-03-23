270aba07b82fee1380b04576b225b2027be17bac
Winsmarl <110825438+winsmarlcanete@users.noreply.github.com>
2025-03-23 18:03:14 +0800

    working simple retrieval of values from sample database

--- src/main/java/Config/JDBC.java
+++ src/main/java/Config/JDBC.java
@@ -1,27 +1,48 @@
 package Config;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class JDBC {
     private final static String URL = "jdbc:mysql://127.0.0.1:3306/sample_schema";
     private final static String USER = "root";
     private final static String PASSWORD = "wpc";
-    public static Connection getConnection() {
+    public static void closeResources(Connection conn, Statement stmt, ResultSet rs) {
         try {
-            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Connected to database!");
+            if (rs != null) rs.close();
+            if (stmt != null) stmt.close();
+            if (conn != null) conn.close();  // Properly close connection
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static Connection getConnection() {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+             conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             stmt  = conn.createStatement();
+             rs = stmt.executeQuery("Select * from sample_table");
+
+            while (rs.next()){
+                System.out.println(rs.getString("sample_tablecol"));
+
+            }
             return conn;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
+        }finally {
+            closeResources(conn,stmt, rs); // Always close resources
         }
+
     }
 
     public static void main(String[] args) {
-        getConnection();  // Test connection
+        getConnection();
+
+
     }
 
 }
