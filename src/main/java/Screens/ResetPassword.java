3e3abd96ffc802bff363eafc53e40dd94aab9bf2
Winsmarl <110825438+winsmarlcanete@users.noreply.github.com>
2025-06-10 22:52:36 +0800

    fixed duplicated conflicts

--- src/main/java/Screens/ResetPassword.java
+++ src/main/java/Screens/ResetPassword.java
@@ -189,31 +189,7 @@ class RoundedButton extends JButton {
 }
 
 // ----------- RoundedTextField Class ------------
-class RoundedTextField extends JTextField {
-    private int cornerRadius = 15;
 
-    public RoundedTextField(int columns) {
-        super(columns);
-        setOpaque(false);
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g.create();
-        g2.setColor(getBackground());
-        g2.fillRoundRect(0, 0, getWidth(), getHeight(), cornerRadius, cornerRadius);
-        super.paintComponent(g2);
-        g2.dispose();
-    }
-
-    @Override
-    protected void paintBorder(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g.create();
-        g2.setColor(Color.GRAY);
-        g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, cornerRadius, cornerRadius);
-        g2.dispose();
-    }
-}
 
 // ----------- RoundedPasswordField Class ------------
 class RoundedPasswordField extends JPasswordField {
