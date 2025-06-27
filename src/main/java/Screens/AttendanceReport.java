a09b47f868b83bbf4d461de204e0e4dd54348e2b
Winsmarl <110825438+winsmarlcanete@users.noreply.github.com>
2025-06-27 23:49:33 +0800

    Add methods for calculating payroll totals by period and department

--- src/main/java/Screens/AttendanceReport.java
+++ src/main/java/Screens/AttendanceReport.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class AttendanceReport extends JPanel {
     public AttendanceReport() {
-        JLabel label = new JLabel("Absent ka nanaman!");
+        JLabel label = new JLabel("");
         label.setFont(new Font("Arial", Font.BOLD, 50));
         this.add(label);
     }
